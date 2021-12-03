be32403c5103575b4af157021881f4c081770a17
Will Briggs <will.briggs@coxautoinc.com>
2021-12-03 13:21:02 -0500

    More cleanup

--- src/main/scala/day1/Part1.scala
+++ src/main/scala/day1/Part1.scala
@@ -1,18 +1,21 @@
 package org.subourbonite
 package day1
 
+import scala.io.Source
+import scala.util.Using
+
 object Part1 {
   def main(args: Array[String]): Unit = {
-    val numIncreases = scala.io.Source
-      .fromURL(getClass.getResource("/day1/input.txt"))
-      .getLines
-      .map(_.toInt)
-      .sliding(2, 1)
-      .count {
-        case Seq(x, y) => x < y
-        case _         => false
-      }
+    val numIncreases = Using(Source.fromURL(getClass.getResource("/day1/input.txt"))) { input =>
+      input.getLines
+        .map(_.toInt)
+        .sliding(2, 1)
+        .count {
+          case Seq(x, y) => x < y
+          case _         => false
+        }
+    }
 
-    println(s"Input file had $numIncreases value increases")
+    println(s"Input file had ${numIncreases.get} value increases")
   }
 }
--- src/main/scala/day1/Part2.scala
+++ src/main/scala/day1/Part2.scala
@@ -1,22 +1,24 @@
 package org.subourbonite
 package day1
 
+import scala.io.Source
+import scala.util.Using
+
 object Part2 {
   def main(args: Array[String]): Unit = {
-    val numIncreases = scala.io.Source
-      .fromURL(getClass.getResource("/day1/input.txt"))
-      .getLines
-      .map(_.toInt)
-      .sliding(3, 1)
-      .map(_.sum)
-      .sliding(2, 1)
-      .count {
-        case Seq(x, y) => x < y
-        case _         => false
-      }
-
+    val numIncreases = Using(Source.fromURL(getClass.getResource("/day1/input.txt"))) { input =>
+      input.getLines
+        .map(_.toInt)
+        .sliding(3, 1)
+        .map(_.sum)
+        .sliding(2, 1)
+        .count {
+          case Seq(x, y) => x < y
+          case _         => false
+        }
+    }
     println(
-      s"Input file had $numIncreases value increases using a sliding window of 3 aggregate values"
+      s"Input file had ${numIncreases.get} value increases using a sliding window of 3 aggregate values"
     )
   }
 }
